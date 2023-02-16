ac67150484367b23e991f30b53236c1500db964b
Thierry <marcelinthierry@gmail.com>
2023-02-15 22:34:54 -0500

    Introduce JWT to app

--- src/main/java/com/thierry/marcelin/bloggingapp/security/JwtTokenProvider.java
+++ src/main/java/com/thierry/marcelin/bloggingapp/security/JwtTokenProvider.java
@@ -1,10 +1,15 @@
 package com.thierry.marcelin.bloggingapp.security;
 
+import com.thierry.marcelin.bloggingapp.exceptions.BlogAPIException;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -53,6 +58,21 @@ public class JwtTokenProvider {
 
     // Validate JWT token
     public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        }catch (MalformedJwtException e){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
+        }catch (ExpiredJwtException e){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Expired JWT token");
+        }catch (UnsupportedJwtException e){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Unsupported JWT token");
+        }catch (IllegalArgumentException e){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "JWT claims string is empty");
 
+        }
     }
 }
